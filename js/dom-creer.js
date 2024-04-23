1631e2b78138fdc7a7e23cbcebd7789e7e481655
unknown <nduflot@dawan.fr>
2024-04-23 16:16:49 +0200

    Javascript creer dom pt2 exo preventdefault()

--- js/dom-creer.js
+++ js/dom-creer.js
@@ -2,8 +2,34 @@ window.addEventListener('DOMContentLoaded', function(){
     const tabEventTarget = document.querySelector('table.tabEventTarget');
     tabEventTarget.addEventListener('click', function(event){
         const line = event.target.parentNode;
-        const col = event.target;
-        console.log(line.dataset);
-        console.log(col.dataset);
+        const column = event.target;
+        const conteneur = document.getElementById('conteneur');
+        
+        let tr = null;
+
+        if(conteneur.lastChild.nodeName === 'TR'){
+            if(conteneur.lastChild.children.length === 5){
+                tr = document.createElement('tr');
+                conteneur.appendChild(tr);
+            }else{
+                tr = conteneur.lastChild;
+            }
+        }else{
+            tr = document.createElement('tr');
+            conteneur.appendChild(tr);
+        }
+        
+        const td = document.createElement('td');
+        td.setAttribute('data-col', column.dataset.col);
+        td.setAttribute('data-row', line.dataset.line);
+        const content = document.createTextNode(`${column.dataset.col} - ${line.dataset.line}`);
+        td.addEventListener('click', function(){
+            document.getElementById('coordonnees').innerText = this.innerText;
+        });
+        td.appendChild(content);
+        tr.append(td);
+        
+        /* preventdefault() */
+
     });
 });
