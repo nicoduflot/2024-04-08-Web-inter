a1be471c144507314230f536a0cecccef4411836
unknown <nduflot@dawan.fr>
2024-04-24 11:08:32 +0200

    Javascript dom css pt3 exobouger le carre

--- js/dom-css.js
+++ js/dom-css.js
@@ -1,3 +1,29 @@
+
+
+/*  la page est chargée */
 loaded(function(){
+    s('#btn').addEventListener('click', function(){
+        const cible = s('#cible');
+        if(cible.hasAttribute('style')){
+            cible.removeAttribute('style');
+            console.log(cible.style.cssText);
+        }else{
+            /*
+            cible.style.borderWidth = '2px';
+            cible.style.borderColor = 'darkmagenta';
+            cible.style.borderStyle = 'dashed';
+            */
+            /*
+            cible.style.setProperty('border-width', '2px');
+            cible.style.setProperty('border-color', 'darkmagenta');
+            cible.style.setProperty('border-style', 'dashed');
+            */
+            cible.style.cssText = 'border-width: 2px; border-color: darkmagenta; border-style: dashed;';
+            console.log(cible.style.cssText);
+
+        }
+    });
+
+    /* faire bouger le carre */
     
 });
