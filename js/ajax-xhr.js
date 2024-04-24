3b4439bd3d112c3f76f9910af8455b4f0670db73
unknown <nduflot@dawan.fr>
2024-04-24 15:49:49 +0200

    Javascript ajax pt 2 xhr asynchrone

--- js/ajax-xhr.js
+++ js/ajax-xhr.js
@@ -36,6 +36,23 @@ loaded(function(){
             console.log(xhr);
             console.log(xhr.responseText);
             */
+
+            xhr.onreadystatechange = function(){
+                if(xhr.status !== 200){
+                    if(xhr.status === 404){
+                        s('#reponse').innerHTML = `Erreur 404 : <br /> la ressource "${urlRessource}" n'existe pas à l'adresse indiquée`;
+                    }else{
+                        s('#reponse').innerHTML = `Erreur ${xhr.status} : <br /> impossibilité de résoudre la requête à l'adresse "${urlRessource}"`;
+                    }
+                }else{
+                    s('#reponse').innerHTML = xhr.responseText;
+                }
+            }
+
+            xhr.open('GET', urlRessource);
+
+            xhr.send();
+
         });
     });
 });
