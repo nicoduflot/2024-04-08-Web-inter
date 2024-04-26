4522862dcf6f910baddbe1eca8cacf023d4b186e
unknown <nduflot@dawan.fr>
2024-04-26 12:28:18 +0200

    Javascript TD pt 2 : structurer les classes d'Aventure

---
+++ js/Aventure/Aventure.js
@@ -0,0 +1,10 @@
+import loaded, {s, sA, randomize} from "./Modules/Utils.js";
+
+
+/* début de l'aventure */
+loaded(function(){
+    
+    /*console.log(randomize(1, 20));*/
+
+    
+});
---
+++ js/Aventure/Modules/Utils.js
@@ -0,0 +1,53 @@
+/**
+ * Fonction raccourci pour ne plus avoir a écrire window.addEventListener('click', function(){})
+ * @param {*} callbackFunction - fonction de rappel a éxécuter une fois que le DOM est entièrement chargé
+ */
+export default function loaded(callbackFunction){
+    window.addEventListener('DOMContentLoaded', callbackFunction);
+}
+
+/**
+ * Raccourcis de querySelector
+ * @param {string} selector - selector : sélecteur CSS de l'élément recherché
+ * @returns 
+ */
+export function s(selector){
+    return document.querySelector(selector);
+}
+
+/**
+ * Raccourcis de querySelectorAll
+ * @param {string} selector  - sélecteur CSS des éléments recherchés
+ * @returns 
+ */
+export function sA(selector){
+    return document.querySelectorAll(selector);
+}
+
+/**
+ * Raccourcis de querySelector && querySelectorAll
+ * @param {string} selector - sélecteur CSS du ou des éléments recherchés
+ * @returns 
+ */
+export function q(selector){
+    const collection = document.querySelectorAll(selector);
+    return (collection.length > 1)? collection : collection[0];
+}
+
+/**
+ * Génère un entier compris entre [min , max]
+ * @param {number} min 
+ * @param {number} max 
+ * @returns
+ */
+export function randomize(min = 0, max = 0){
+    if(isNaN(parseFloat(min)) || isNaN(parseFloat(max))){
+        return 0;
+    }else{
+        if(parseFloat(max) <= parseFloat(min)){
+            return 0;
+        }else{
+            return Math.floor(Math.random() * (parseFloat(max) - parseFloat(min) + 1)) + parseFloat(min);
+        }
+    }
+}
